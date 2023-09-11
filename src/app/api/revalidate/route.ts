6f9e060338b2f097df1e11a5985d2a54f24e008c
JClark <justindanielclark@gmail.com>
2023-09-11 09:51:56 -0700

    removed console log

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -5,7 +5,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
 export async function GET(request: NextRequest) {
-  console.log("hit");
   const secret = request.nextUrl.searchParams.get("secret");
   if (secret !== process.env.MY_SECRET_TOKEN) {
     return new NextResponse(JSON.stringify({ message: "Invalid Token" }), {
